4be82c14230a1b96f1e56612083f08b52f54c45b
Seasawher <tellagoodstory@icloud.com>
2024-06-17 15:16:06 +0900

    クリックすると背景も色が変わるように変更を加えた

--- src/bin/color_change.rs
+++ src/bin/color_change.rs
@@ -1,10 +1,14 @@
-/* クリックすると色を変える正方形 */
+/* クリックすると正方形と背景の色がそれぞれランダムに変わる
+
+ただし画面のどこをクリックしても色が変わる
+*/
 
 use nannou::prelude::*;
 
 // 現在設定している色の状態を保持
 struct Model {
-  color: Rgb8,
+  rec_color: Rgb8,
+  back_color: Rgb8,
 }
 
 fn main() {
@@ -23,13 +27,15 @@ fn model(app: &App) -> Model {
 
   // 色の初期値を指定してインスタンス生成
   Model {
-    color: gen_random_color(),
+    rec_color: gen_random_color(),
+    back_color: gen_random_color(),
   }
 }
 
 // クリック時のイベントハンドラ
 fn mouse_pressed(_app: &App, model: &mut Model, _button: MouseButton) {
-  model.color = gen_random_color()
+  model.rec_color = gen_random_color();
+  model.back_color = gen_random_color();
 }
 
 // ランダムな色を返す自作関数
@@ -44,12 +50,12 @@ fn gen_random_color() -> Rgb8 {
 fn view(app: &App, model: &Model, frame: Frame) {
   let draw = app.draw();
 
-  draw.background().color(WHITE);
+  draw.background().color(model.back_color);
 
   draw
     .rect()
     // 設定した色で塗りつぶす
-    .color(model.color)
+    .color(model.rec_color)
     .x_y(0.0, 0.0)
     .w_h(100.0, 100.0);
 
